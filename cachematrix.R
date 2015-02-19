24908b9a01eabbe0842651ab7e927774c40ed9d6
emdill <erin.dillon@gmail.com>
2015-02-19 13:10:47 -0500

    First Commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Create a matrix that can chache its inverse, then return the chache if calculated, if not, calculate the inverse and chache it.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) { #Sets the value of the matrix
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x #Gets the value of the matrix
+  setmatrix <- function(solve) m <<- solve #Sets the value of the inverse matrix
+  getmatrix <- function() m #Gets the value of the inverse matrix
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix) #Calls functions to set & get the inverse matrix value
 }
 
+## Checks if inverse matrix is chached. If it is not cached, calculate and return the inverse.
 
-## Write a short comment describing this function
+cacheSolve <- function(x=matrix(), ...) {
+        m <- x$getmatrix()
+        if(!is.null(m)) { #Check if inverse is already calculated
+            message("getting cached matrix")
+            return(m) #If it is, return chached matrix
+        }
+        matrix <- x$get()
+        m <- solve(matrix, ...) #If inverse is not chached, calculate inverse
+        x$setmatrix(m) #Set value of cache to inverse matrix
+        m        ## Return a matrix that is the inverse of 'x'
+    }
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+
